9350ccae4d23efa27a6013a1ba29514a254d9a08
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-07-01 22:28:02 -0400

    work on shape js building the primeric of each shapes

--- examples/lib/shapes.js
+++ examples/lib/shapes.js
@@ -1,17 +1,54 @@
 // write a class for shape
 class Shape {
-    constructor(){this.color = ""}
+    constructor(){
+        this.color = ""
+        this.text = ""
+    }
+
     setColor(color){this.color = color}
+    setText(text){this.text = text}
 }
 
-class Circle extends Shape {} 
+class Circle extends Shape {
+    render(){
+        return`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
 
-class Square extends Shape {}
+<circle cx="150" cy="100" r="80" fill="${this.color}"/>
 
-class Triangle extends Shape {}
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">${this.text}</text>
+
+</svg>`
+    }
+} 
+
+class Square extends Shape {
+    render(){
+        return`<rect x="10" y="10" width="30" height="30"/>
+<rect x="60" y="10" rx="10" ry="10" width="30" height="30"/>`
+    }
+}
+
+class Triangle extends Shape {
+    render() {
+        return``
+    }
+}
 
 console.log("shape"); 
 
 
 // constructor arithmetic is exported from the file.
 module.exports = shapes.test.js;
+
+
+
+
+
+
+{/* <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+
+<circle cx="150" cy="100" r="80" fill="green"/>
+
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+</svg> */}
--- examples/lib/shapes.test.js
+++ examples/lib/shapes.test.js
@@ -1,8 +1,12 @@
 const shapes = require('../lib/shapes.test.js');
 
+// TODO: CREATED MY TEST
 describe('shapes', () => {
+ 
+describe('i expect when shape is call i will get a circle, triangle, or square', () => {
+    it('should take a shape and get a circle, triangle or a square', () => {
+        
+        expect(shapes.)
+    })
 });
-
-describe('i expect when shape is call i will get a circle, triangle, or swuare', () => {
-
-})
+});
